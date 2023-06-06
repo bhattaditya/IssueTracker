23522756f8b8cb11e1a727af39c4be7e6df10eba
Aditya Bhatt <bhattaditya2@gmail.com>
2023-06-06 10:12:22 +0530

    active issues count method implement

--- src/main/java/com/bhattaditya/issuetracker/service/AssigneeService.java
+++ src/main/java/com/bhattaditya/issuetracker/service/AssigneeService.java
@@ -2,6 +2,7 @@ package com.bhattaditya.issuetracker.service;
 
 import com.bhattaditya.issuetracker.entity.Assignee;
 import com.bhattaditya.issuetracker.entity.Unit;
+import com.bhattaditya.issuetracker.exception.IssueTrackerException;
 
 import java.util.List;
 
@@ -9,7 +10,7 @@ public interface AssigneeService {
 
     void createAssignee(Assignee assignee);
     List<Assignee> fetchAssignees(Unit unit);
-    void updateActiveIssueCount(String assigneeEmail, Character operation);
+    void updateActiveIssueCount(String assigneeEmail, Character operation) throws IssueTrackerException;
 
     void deleteAssignee();
 
--- src/main/java/com/bhattaditya/issuetracker/service/impl/AssigneeServiceImpl.java
+++ src/main/java/com/bhattaditya/issuetracker/service/impl/AssigneeServiceImpl.java
@@ -2,12 +2,14 @@ package com.bhattaditya.issuetracker.service.impl;
 
 import com.bhattaditya.issuetracker.entity.Assignee;
 import com.bhattaditya.issuetracker.entity.Unit;
+import com.bhattaditya.issuetracker.exception.IssueTrackerException;
 import com.bhattaditya.issuetracker.repository.AssigneeRepository;
 import com.bhattaditya.issuetracker.service.AssigneeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -28,8 +30,18 @@ public class AssigneeServiceImpl implements AssigneeService {
     }
 
     @Override
-    public void updateActiveIssueCount(String assigneeEmail, Character operation) {
+    public void updateActiveIssueCount(String assigneeEmail, Character operation) throws IssueTrackerException {
+        Optional<Assignee> assignee = assigneeRepository.findByEmail(assigneeEmail);
+        if (assignee.isEmpty()) {
+            throw new IssueTrackerException("Not Assignee found with email : " + assigneeEmail);
+        }
+        Integer numberOfActiveIssues = assignee.get().getNumberOfActiveIssues();
 
+        if (operation == 'I') {
+            assignee.get().setNumberOfActiveIssues(numberOfActiveIssues + 1);
+        } else if (operation == 'D') {
+            assignee.get().setNumberOfActiveIssues(numberOfActiveIssues - 1);
+        }
     }
 
     @Override
